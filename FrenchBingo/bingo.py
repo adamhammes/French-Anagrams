7c5732e674a4445842ce0ffcb7501b8287132aa4
Adam Hammes <adam@hammes.io>
2015-10-31 00:57:32 -0500

    Spaces -> tabs, correctly strip whitespace and look for words of all lengths

--- FrenchBingo/bingo.py
+++ FrenchBingo/bingo.py
@@ -4,16 +4,24 @@ import unicodedata
 
 
 def remove_accent(word):
-    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+	return ''.join(c for c in unicodedata.normalize('NFD', word) if unicodedata.category(c) != 'Mn')
 
 
-def read_file(filename, encoding='utf-8', max_word_length=10):
-    with codecs.open(filename, 'r', encoding) as f:
-        raw_lines = f.readlines()
-        
-    no_accents = [remove_accent(line) for line in raw_lines]
-    return frozenset([word for word in no_accents if len(word) <= max_word_length]) 
-    
+def read_file(filename, encoding='utf-8', max_word_length=100):
+	with codecs.open(filename, 'r', encoding) as f:
+		with_accents = [line.strip() for line in f.readlines()]
 
-def find_words(word_list, letters):
-    return word_list & set(itertools.permutations(letters))
+	no_accents = [remove_accent(word) for word in with_accents]
+	return frozenset([word for word in no_accents if len(word) <= max_word_length]) 
+	
+
+def find_words(word_list, letters, sort_by_length=False):
+	possibilities = set() 
+	for length in range(0, len(letters) + 1):
+		possibilities |= set([''.join(p) for p in itertools.permutations(letters, length)])
+
+	results = list(word_list & possibilities)
+	if sort_by_length:
+		results.sort(key=len, reverse=True) 
+
+	return(results)
