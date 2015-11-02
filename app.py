cb0ea4d414b880fcc266065f5a21d87db66b0f4f
Adam Hammes <adam@hammes.io>
2015-11-02 17:54:23 -0600

    Moved length check to correct spot

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from FrenchBingo import bingo
 
+MAX_WORD_LENGTH = 7
+
+
 app = Flask(__name__, static_url_path='/static/')
 words = bingo.read_file('FrenchBingo/words.txt')
 
@@ -10,12 +13,13 @@ def hello():
     if request.method == 'GET':
         return app.send_static_file('home.html')
     else:
-        letters = request.form['letters'][:7]
+        letters = request.form['letters']
         return redirect(url_for('run', letters=letters))
 
 
 @app.route("/<letters>")
 def run(letters):
+    letters = letters[:MAX_WORD_LENGTH]
     results = bingo.find_words(words, letters, sort_by_length=True)[:15]
     return render_template('results.html', letters=letters, results=results)
 
