7a31d781ab06bf7d9d4fef61f21a1f37ece1654d
Adam Hammes <adam@hammes.io>
2015-11-02 16:12:50 -0600

    Added back REST type urls

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from FrenchBingo import bingo
 
 app = Flask(__name__, static_url_path='/static/')
@@ -11,8 +11,13 @@ def hello():
         return app.send_static_file('home.html')
     else:
         letters = request.form['letters']
-        results = bingo.find_words(words, letters, sort_by_length=True)[:15]
-        return render_template('results.html', letters=letters, results=results)
+        return redirect(url_for('run', letters=letters))
+
+
+@app.route("/<letters>")
+def run(letters):
+    results = bingo.find_words(words, letters, sort_by_length=True)[:15]
+    return render_template('results.html', letters=letters, results=results)
 
 if __name__ == '__main__':
     app.run()
